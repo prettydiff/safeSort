9d91fbc5b5dcf60b5b7f5bbe1de98fdb789c0a20
Austin Cheney <austin.cheney@orbitz.com>
2015-04-24 11:42:23 -0500

    new algorithm

--- safeSort.js
+++ safeSort.js
@@ -10,9 +10,22 @@ It only takes two parameters:
     - "normal" - will normalize (remove redundant values) from the array
     - "descend" - will order the array from greatest values to lowest values
     - "ascend" - will order the array from lowest valus to greatest values
-
+* recursive - accepts a boolean and defaults to false. If true then any indexes
+    that contain arrays will be independently sorted and returned into their
+    proper location
 The operation parameter is optional. If not specified the value "ascend"
 will be assumed.
+
+
+How the algorithm works:
+
+1) Walk the entire array looking the lowest value and lowest type and count the
+   instances.
+2) Note which index(es) in the array contain this lowest value, x indexes
+3) Swap the values of the first x indexes with the low indexes
+4) Keep aware of the length of low values in a closure... variable c
+5) Do it all over again with one exception:
+      * Start walking the array from index c
 
 Enjoy and visit http://prettydiff.com/
 
@@ -21,102 +34,160 @@ MIT license
 Copyright 2015 Austin Cheney
 
 */
-var safeSort = function (array, operation) {
-        "use strict";
-        var normal  = function () {
-                var done    = [array[0]],
-                    storeb  = array,
-                    recurse = function (x) {
-                        var a      = 0,
-                            storea = [],
-                            len    = storeb.length;
-                        for (a = 0; a < len; a += 1) {
-                            if (storeb[a] !== x) {
-                                storea.push(storeb[a]);
-                            }
+var safeSort = function (array, operation, recursive) {
+    "use strict";
+    var arTest  = function (item) {
+            if (typeof item !== "object" || item.length === undefined || item.length < 2) {
+                return false;
+            }
+            return true;
+        },
+        normal  = function (item) {
+            var done    = [item[0]],
+                storeb  = item,
+                child   = function () {
+                    var a   = 0,
+                        len = storeb.length;
+                    for (a = 0; a < len; a += 1) {
+                        if (arTest(storeb[a]) === true) {
+                            storeb[a] = normal(storeb[a]);
                         }
-                        storeb = storea;
-                        if (storea.length > 0) {
-                            done.push(storea[0]);
-                            recurse(storea[0]);
-                        } else {
-                            array = done;
+                    }
+                },
+                recurse = function (x) {
+                    var a      = 0,
+                        storea = [],
+                        len    = storeb.length;
+                    for (a = 0; a < len; a += 1) {
+                        if (storeb[a] !== x) {
+                            storea.push(storeb[a]);
                         }
-                    };
-                recurse(array[0]);
-            },
-            descend = function () {
-                var done    = [],
-                    storeb  = array,
-                    recurse = function () {
-                        var a      = 0,
-                            b      = 0,
-                            ind    = [],
-                            len    = storeb.length,
-                            min    = storeb[0];
-                        for (a = 0; a < len; a += 1) {
-                            if (storeb[a] > min || typeof storeb[a] > typeof min) {
-                                min = storeb[a];
-                                ind = [a];
-                            } else if (storeb[a] === min) {
-                                ind.push(a);
-                            }
+                    }
+                    storeb = storea;
+                    if (storea.length > 0) {
+                        done.push(storea[0]);
+                        recurse(storea[0]);
+                    } else {
+                        if (recursive === true) {
+                            item = child();
                         }
-                        b = ind.length;
-                        for (a = b - 1; a > -1; a -= 1) {
-                            done.push(min);
-                            storeb.splice(ind[a], 1);
+                        item = storeb;
+                    }
+                };
+            recurse(array[0]);
+        },
+        descend = function (item) {
+            var c       = 0,
+                storeb  = item,
+                len     = item.length,
+                child   = function () {
+                    var a    = 0,
+                        lenc = storeb.length;
+                    for (a = 0; a < lenc; a += 1) {
+                        if (arTest(storeb[a]) === true) {
+                            storeb[a] = descend(storeb[a]);
                         }
-                        if (storeb.length > 0) {
-                            recurse();
-                        } else {
-                            array = done;
+                    }
+                },
+                recurse = function () {
+                    var a   = 0,
+                        b   = 0,
+                        d   = 0,
+                        e   = 0,
+                        ind = [],
+                        key = storeb[c];
+                    for (a = c; a < len; a += 1) {
+                        if (storeb[a] > key || typeof storeb[a] > typeof key) {
+                            key = storeb[a];
+                            ind = [a];
+                        } else if (storeb[a] === key) {
+                            ind.push(a);
                         }
-                    };
-                recurse();
-            },
-            ascend  = function () {
-                var done    = [],
-                    storeb  = array,
-                    recurse = function () {
-                        var a      = 0,
-                            b      = 0,
-                            ind    = [],
-                            len    = storeb.length,
-                            min    = storeb[0];
-                        for (a = 0; a < len; a += 1) {
-                            if (storeb[a] < min || typeof storeb[a] < typeof min) {
-                                min = storeb[a];
-                                ind = [a];
-                            } else if (storeb[a] === min) {
-                                ind.push(a);
-                            }
+                    }
+                    d = ind.length;
+                    b = d + c;
+                    for (a = c; a < b; a += 1) {
+                        storeb[ind[e]] = storeb[a];
+                        storeb[a]      = key;
+                        e              += 1;
+                    }
+                    c += d;
+                    if (c < len) {
+                        recurse();
+                    } else {
+                        if (recursive === true) {
+                            item = child();
                         }
-                        b = ind.length;
-                        for (a = b - 1; a > -1; a -= 1) {
-                            done.push(min);
-                            storeb.splice(ind[a], 1);
+                        item = storeb;
+                    }
+                };
+            recurse();
+            return item;
+        },
+        ascend  = function (item) {
+            var c       = 0,
+                storeb  = item,
+                len     = item.length,
+                child   = function () {
+                    var a    = 0,
+                        lenc = storeb.length;
+                    for (a = 0; a < lenc; a += 1) {
+                        if (arTest(storeb[a]) === true) {
+                            storeb[a] = ascend(storeb[a]);
                         }
-                        if (storeb.length > 0) {
-                            recurse();
-                        } else {
-                            array = done;
+                    }
+                },
+                recurse = function () {
+                    var a   = 0,
+                        b   = 0,
+                        d   = 0,
+                        e   = 0,
+                        ind = [],
+                        key = storeb[c];
+                    for (a = c; a < len; a += 1) {
+                        if (storeb[a] < key || typeof storeb[a] < typeof key) {
+                            key = storeb[a];
+                            ind = [a];
+                        } else if (storeb[a] === key) {
+                            ind.push(a);
                         }
-                    };
-                recurse();
-            };
-        if (typeof array !== "object" || array.length === undefined || array.length < 2) {
-            return array;
-        }
-        if (operation === "normal") {
-            normal();
-        } else if (operation === "descend") {
-            descend();
-        } else {
-            ascend();
-        }
+                    }
+                    d = ind.length;
+                    b = d + c;
+                    for (a = c; a < b; a += 1) {
+                        storeb[ind[e]] = storeb[a];
+                        storeb[a]      = key;
+                        e              += 1;
+                    }
+                    c += d;
+                    if (c < len) {
+                        recurse();
+                    } else {
+                        if (recursive === true) {
+                            child();
+                        }
+                        item = storeb;
+                    }
+                };
+            recurse();
+            return item;
+        };
+    if (arTest(array) === false) {
         return array;
-    };
+    }
+    if (recursive === "true") {
+        recursive = true;
+    } else if (recursive !== true) {
+        recursive = false;
+    }
+    if (operation === "normal") {
+        return normal(array);
+    }
+    if (operation === "descend") {
+        return descend(array);
+    }
+    return ascend(array);
+};
 if (typeof exports === "object" || typeof exports === "function") { //commonjs and nodejs support
     exports.safeSort = function commonjs(array, operation) {
         "use strict";
